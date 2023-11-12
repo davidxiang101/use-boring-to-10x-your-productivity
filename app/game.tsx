e373dff2b1a82ce9454edec44e28b867a9e4a6f9
davidxiang101 <davidxiang101@gmail.com>
2023-11-12 17:03:36 -0500

    homepage

---
+++ app/game.tsx
@@ -0,0 +1,9 @@
+export default function Game() {
+    return (
+        <div className="game-container">
+            <h1>Color Sorting Game</h1>
+            <p>Welcome to the game. Sort the circles by color.</p>
+            {/* Game implementation will go here */}
+        </div>
+    );
+}
